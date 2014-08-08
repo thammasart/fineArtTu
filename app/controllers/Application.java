d73e4f83c3b5494cfbed6370a90618205da7c5e6
peat2110 <pppppooo@hotmail.com>
2014-08-08 16:12:18 +0700

    html loginPage

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,23 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import play.data.*;
+import play.libs.Json;
 import views.html.*;
+import models.*;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(login.render());
+    }
+    
+    @Security.Authenticated(Secured.class)
+    public static Result authentication(){
+        return TODO;
     }
 
 }
