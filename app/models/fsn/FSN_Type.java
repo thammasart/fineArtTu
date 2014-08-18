a7723c5f2b965d337020d57ec6627d22b6742394
soranad <oldman.beta@gmail.com>
2014-08-18 17:14:29 +0700

    add model FSN and supplies

---
+++ app/models/fsn/FSN_Type.java
@@ -0,0 +1,18 @@
+package models.fsn;
+import play.db.ebean.*;
+import javax.persistence.*;
+
+@Entity
+public class FSN_Type extends Model{
+
+	@Id
+	public String typeId;
+	@Column(nullable=false)
+	public String typeDescription;
+
+	@ManyToOne
+	public FSN_Class groupClass;
+
+	@SuppressWarnings("unchecked")
+	public static Finder<String,FSN_Type> find = new Finder(String.class,FSN_Type.class);
+}
