4b74f2d601a5944f9444c6d1f7743efd216e80ac
peat2110 <pppppooo@hotmail.com>
2014-08-25 16:34:34 +0700

    nomalize css Ui

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,9 +3,17 @@ function validateNumberKey(evt) {
   var theEvent = evt || window.event;
   var key = theEvent.keyCode || theEvent.which;
   key = String.fromCharCode( key );
-  var regex = /[0-9]/;
+
+    if(key == 37 || key == 38 || key == 39 || key == 40 || key == 8 || key == 46) { // Left / Up / Right / Down Arrow, Backspace, Delete keys
+         return;
+     }
+
+  var regex = /[0-9]\b/;
   if( !regex.test(key) ) {
     theEvent.returnValue = false;
     if(theEvent.preventDefault) theEvent.preventDefault();
   }
+}
+function goBack() {
+    window.history.back()
 }
