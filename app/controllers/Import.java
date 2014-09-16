7fecbd01799b12719b5aed71faa923b2942479e7
phantomXIII <ulteriorphantom@gmail.com>
2014-09-16 18:42:53 +0700

    add 3 type of Goods

--- app/controllers/Import.java
+++ app/controllers/Import.java
@@ -35,13 +35,67 @@ public class Import extends Controller {
 
 
     public static Result saveNewInstitute(){
+        DynamicForm form = Form.form().bindFromRequest();
+
+        String typeDurableArticles="";
+        String typeDurableGoods="";
+        String typeConsumableGoods="";
+
+            for(int i=1;i<=17;i++)
+            {
+                String dA = form.get("durableArticlesType"+i);
+                String dG = form.get("durableGoodsType"+i);
+                String cG = form.get("consumableGoodsType"+i);
+
+
+                if(dA!=null)
+                {
+                    typeDurableArticles=typeDurableArticles+dA+",";
+                }
+                if(dG!=null)
+                {
+                    typeDurableGoods=typeDurableGoods+dG+",";
+                }
+                if(cG!=null)
+                {
+                    typeConsumableGoods=typeConsumableGoods+cG+",";
+                }
+
+            }
+
+        
+        typeDurableArticles = typeDurableArticles.length() > 0 ? typeDurableArticles.substring(0,typeDurableArticles.length()-1) : "";
+        typeDurableGoods = typeDurableGoods.length() > 0 ? typeDurableGoods.substring(0,typeDurableGoods.length()-1) : "";
+        typeConsumableGoods = typeConsumableGoods.length() > 0 ? typeConsumableGoods.substring(0,typeConsumableGoods.length()-1) : "";
+
+
         Form<Company> newInstituteFrom = Form.form(Company.class).bindFromRequest();
         Form<Address> newAddressFrom = Form.form(Address.class).bindFromRequest();
+        
         Company newInstitute = newInstituteFrom.get();
+
+        if(form.get("payPeriod2").equals(""))
+            newInstitute.payPeriod=0;
+        else
+            newInstitute.payPeriod=Integer.parseInt(form.get("payPeriod2"));
+        if(form.get("sendPeriod2").equals(""))
+            newInstitute.sendPeriod=0;
+        else
+            newInstitute.sendPeriod=Integer.parseInt(form.get("sendPeriod2"));
+
+        newInstitute.durableArticlesType = typeDurableArticles;
+        newInstitute.durableGoodsType = typeDurableGoods;
+        newInstitute.consumableGoodsType = typeConsumableGoods;
+
+
+
         Address newInstituteaddress = newAddressFrom.get();
         newInstituteaddress.save();
         newInstitute.address = newInstituteaddress;
         newInstitute.save();
+
+
+
         return redirect(routes.Import.importsInstitute());
     }
     
