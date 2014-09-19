e3cc6f6663d36d7be73fcb9fa73fdea22b4c7233
fulkkung <fulkkung@gmail.com>
2014-09-19 11:13:31 +0700

    normalize importOrder.js funtion

--- public/javascripts/controllers/importOrder.js
+++ public/javascripts/controllers/importOrder.js
@@ -7,105 +7,57 @@ $('document').ready(function(){
 	createEOCommittee();
 }); 
 function getCommitteeTemplate(name){
-	var s;
-	if(name == 'ai'){
-		aiLists.push(i);
-		s = '<div id="ai' + i +'">'+
-		'				<input type="text" hidden="true" name="aiLists">'+
-		'				<div class="form-group" >'+
-		'					<div class="input-group" >'+
-		'					    <span class="input-group-addon">คำนำหน้าชื่อ</span>'+
-		'					    <input name="aiPrefixName'+ i +'"  type="text" class="form-control textAlignCenter  width100px"placeholder="ใส่ค่า">'+
-		'					</div>'+
-		'				</div>'+
-		'				<div class="form-group" >'+
-		'					<div class="input-group" >'+
-		'					    <span class="input-group-addon">ชื่อ/สกุล</span>'+
-		'					    <input name="aiName'+ i +'" type="text" class="form-control textAlignCenter  width225px"placeholder="ใส่ค่า">'+
-		'					</div>'+
-		'				</div>'+
-		'				<div class="form-group" >'+
-		'					<div class="input-group" >'+
-		'					    <span class="input-group-addon" >ตำแหน่ง</span>'+
-		'					    <input name="aiPosition'+ i +'" type="text" class="form-control textAlignCenter  width150px"placeholder="ตามข้อมูลuser">'+
-		'					</div>'+
-		'				</div>'+
-		'				'+
-		'				'+
-		'				<div class="form-group" role="form">'+
-		'				    <div class="input-group">'+
-		'						<span class="input-group-addon">ประเภทกรรมการ</span>'+
-		'						<select name="aiCommitteeType'+ i +'" class="form-control textAlignCenter  width150px">'+
-		'						  <option>---เลือก---</option>'+
-		'						  <option>ข้าราชการ</option>'+
-		'						  <option>พนักงานมหาลัย</option>'+
-		'						</select>'+
-		'				    </div>'+
-		'				</div>'+
-		'				'+
-		'				<div class="form-group" role="form">'+
-		'					<div class="input-group">'+
-		'						<span class="input-group-addon">ตำแหน่งในคณะกรรมการ</span>'+
-		'						<select name="aiCommitteePosition'+ i +'" class="form-control textAlignCenter  width200px">'+
-		'						  <option>---เลือก---</option>'+
-		'						  <option>ประธารกรรมการ</option>'+
-		'						  <option>กรรมการ</option>'+
-		'						  <option>กรรมการและเรขานุการ</option>'+
-		'						</select>'+
-		'					</div>'+
-		'				 </div>'+
-		'				 <button type="button" onclick="removeDivCommittee(\'ai\','+ i +')">ลบ</button>'+
-		'			 </div> <!-- id = ai'+ i +' -->'
-		i++;
-	}else if(name == 'eo'){
-		eoLists.push(j);
-		s = '<div id="eo' + j +'">'+
-		'				<div class="form-group" >'+
-		'					<div class="input-group" >'+
-		'					    <span class="input-group-addon">คำนำหน้าชื่อ</span>'+
-		'					    <input name="eoPrefixName'+ j +'"  type="text" class="form-control textAlignCenter  width100px"placeholder="ใส่ค่า">'+
-		'					</div>'+
-		'				</div>'+
-		'				<div class="form-group" >'+
-		'					<div class="input-group" >'+
-		'					    <span class="input-group-addon">ชื่อ/สกุล</span>'+
-		'					    <input name="eoName'+ j +'" type="text" class="form-control textAlignCenter  width225px"placeholder="ใส่ค่า">'+
-		'					</div>'+
-		'				</div>'+
-		'				<div class="form-group" >'+
-		'					<div class="input-group" >'+
-		'					    <span class="input-group-addon" >ตำแหน่ง</span>'+
-		'					    <input name="eoPosition'+ j +'" type="text" class="form-control textAlignCenter  width150px"placeholder="ตามข้อมูลuser">'+
-		'					</div>'+
-		'				</div>'+
-		'				'+
-		'				'+
-		'				<div class="form-group" role="form">'+
-		'				    <div class="input-group">'+
-		'						<span class="input-group-addon">ประเภทกรรมการ</span>'+
-		'						<select name="eoCommitteeType'+ j +'" class="form-control textAlignCenter  width150px">'+
-		'						  <option>---เลือก---</option>'+
-		'						  <option>ข้าราชการ</option>'+
-		'						  <option>พนักงานมหาลัย</option>'+
-		'						</select>'+
-		'				    </div>'+
-		'				</div>'+
-		'				'+
-		'				<div class="form-group" role="form">'+
-		'					<div class="input-group">'+
-		'						<span class="input-group-addon">ตำแหน่งในคณะกรรมการ</span>'+
-		'						<select name="eoCommitteePosition'+ j +'" class="form-control textAlignCenter  width200px">'+
-		'						  <option>---เลือก---</option>'+
-		'						  <option>ประธารกรรมการ</option>'+
-		'						  <option>กรรมการ</option>'+
-		'						  <option>กรรมการและเรขานุการ</option>'+
-		'						</select>'+
-		'					</div>'+
-		'				 </div>'+
-		'				 <button type="button" onclick="removeDivCommittee(\'eo\','+ j +')">ลบ</button>'+
-		'			 </div> <!-- id = eo'+ j +' -->'
-		j++;
-	}
+	name == 'ai' ? aiLists.push(i):eoLists.push(j);
+	var num = name == 'ai' ? i:j;
+	console.log(name == 'ai' ? 'i':'j');
+	var s = '<div id="'+name + num +'">'+
+	'				<input type="text" hidden="true" name="aiLists">'+
+	'				<div class="form-group" >'+
+	'					<div class="input-group" >'+
+	'					    <span class="input-group-addon">คำนำหน้าชื่อ</span>'+
+	'					    <input name="'+name+'PrefixName'+ num +'"  type="text" class="form-control textAlignCenter  width100px"placeholder="ใส่ค่า">'+
+	'					</div>'+
+	'				</div>'+
+	'				<div class="form-group" >'+
+	'					<div class="input-group" >'+
+	'					    <span class="input-group-addon">ชื่อ/สกุล</span>'+
+	'					    <input name="'+name+'Name'+ num +'" type="text" class="form-control textAlignCenter  width225px"placeholder="ใส่ค่า">'+
+	'					</div>'+
+	'				</div>'+
+	'				<div class="form-group" >'+
+	'					<div class="input-group" >'+
+	'					    <span class="input-group-addon" >ตำแหน่ง</span>'+
+	'					    <input name="'+name+'Position'+ num +'" type="text" class="form-control textAlignCenter  width150px"placeholder="ตามข้อมูลuser">'+
+	'					</div>'+
+	'				</div>'+
+	'				'+
+	'				'+
+	'				<div class="form-group" role="form">'+
+	'				    <div class="input-group">'+
+	'						<span class="input-group-addon">ประเภทกรรมการ</span>'+
+	'						<select name="'+name+'CommitteeType'+ num +'" class="form-control textAlignCenter  width150px">'+
+	'						  <option>---เลือก---</option>'+
+	'						  <option>ข้าราชการ</option>'+
+	'						  <option>พนักงานมหาลัย</option>'+
+	'						</select>'+
+	'				    </div>'+
+	'				</div>'+
+	'				'+
+	'				<div class="form-group" role="form">'+
+	'					<div class="input-group">'+
+	'						<span class="input-group-addon">ตำแหน่งในคณะกรรมการ</span>'+
+	'						<select name="'+name+'CommitteePosition'+ num +'" class="form-control textAlignCenter  width200px">'+
+	'						  <option>---เลือก---</option>'+
+	'						  <option>ประธารกรรมการ</option>'+
+	'						  <option>กรรมการ</option>'+
+	'						  <option>กรรมการและเรขานุการ</option>'+
+	'						</select>'+
+	'					</div>'+
+	'				 </div>'+
+	'				 <button type="button" onclick="removeDivCommittee(\''+name+'\','+ num +')">ลบ</button>'+
+	'			 </div> <!-- id ='+ name + num +' -->'
+	name == 'ai' ? i++ : j++;
+
 	return s;
 }
 function createAICommittee() {
@@ -124,13 +76,10 @@ function createEOCommittee(){
 function removeDivCommittee(name,num){
 	if(name == 'ai'){
 		aiLists.remove(num);
-		document.getElementById("ai_committee");
 		document.getElementById("ai"+num).remove();
 		document.getElementById("aiLists").value = aiLists.join();
-		console.log(document.getElementById("aiLists").value);
 	}else if(name == 'eo'){
 		eoLists.remove(num);
-		document.getElementById("ai_committee");
 		document.getElementById("eo"+num).remove();
 		document.getElementById("eoLists").value = eoLists.join();
 	}
