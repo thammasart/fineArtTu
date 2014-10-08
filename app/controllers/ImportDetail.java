2f6b447d7b44fdb5affd27aa3aad7e668397a7ee
phantomXIII <ulteriorphantom@gmail.com>
2014-10-08 19:09:32 +0700

    add ability to see detail and fix detail of fsnDetail

--- app/controllers/ImportDetail.java
+++ app/controllers/ImportDetail.java
@@ -46,10 +46,7 @@ public class ImportDetail extends Controller {
 	
 	@Security.Authenticated(Secured.class)
     public static Result importsInstituteDetail(long id) {
-		System.out.println(id);
         User user = User.find.where().eq("username", session().get("username")).findUnique();
-        
-        
         
         Company c = Company.find.byId(id);
         
@@ -152,4 +149,29 @@ public class ImportDetail extends Controller {
 	        return redirect(routes.Import.importsInstitute());
 	    }
 
+	 @Security.Authenticated(Secured.class)
+	 public static Result importsMaterialDurableArticlesDetail(String id)
+	 {
+		User user = User.find.where().eq("username", session().get("username")).findUnique();
+		List<FSN_Type> groupType = FSN_Type.find.all(); 
+		FSN_Description fsn = FSN_Description.find.byId(id);
+		
+		System.out.println(fsn.descriptionId);
+		System.out.println(fsn.typ.typeId);
+		
+		return ok(importsMaterialDurableArticlesDetail.render(groupType,user,fsn));
+	 }
+	 
+	 
+	 public static Result saveNewMaterialDurableArticles(){
+		 DynamicForm form = Form.form().bindFromRequest();
+		 
+		 FSN_Description fd = FSN_Description.find.byId(form.get("descriptionId"));
+		 fd.otherDetail = form.get("otherDetail");
+		 
+		 fd.update();
+		 
+		 return redirect(routes.Import.importsMaterial());
+		 
+	 }
 }
