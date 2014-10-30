1c267ae09c8818411840de49896fc0ce5ea77e1e
soranad <oldman.beta@gmail.com>
2014-10-30 14:59:09 +0700

    delete borrow export

--- public/javascripts/export/exportBorrow.js
+++ public/javascripts/export/exportBorrow.js
@@ -23,3 +23,38 @@ var deleteModalInstanceCtrl = function($scope, $modalInstance){
     };
 }
 
+var deleteList = [];
+
+function addDeleteList(code){
+    if(deleteList.indexOf(code) > -1){
+        deleteList.remove(code);
+        document.getElementById("row" + code).style.color = "";
+        document.getElementById("checkbox" + code).checked = false;
+    }
+    else{
+        deleteList.push(code);
+        document.getElementById("row" + code).style.color = "#cc3300";
+        document.getElementById("checkbox" + code).checked = true;
+    }
+}
+
+function deleteBorrow(){
+    var dataDetail = {};
+    dataDetail.detail = deleteList;
+    $.ajax({
+        url:'/export/borrow/deletes',
+        type: 'post',
+        data: JSON.stringify(dataDetail),
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function(result){
+            var status = result["status"];
+            if(status == "SUCCESS"){
+                var deleteList = [];
+            }
+            else{
+                alert('save detail error : ' + data["message"]);
+            }
+        }
+    });
+}
