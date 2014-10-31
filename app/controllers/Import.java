d57bf695238c0da5458be278721ac02d305c00a7
phantomXIII <ulteriorphantom@gmail.com>
2014-10-31 19:22:54 +0700

    fix ดับเบิ้ล

--- app/controllers/Import.java
+++ app/controllers/Import.java
@@ -1186,6 +1186,7 @@ public class Import extends Controller {
 	    	
 	    	
 	    	if(!codeId.equals("")){
+	    		
 	    		procurementDetail.code = codeId; //fsn or code 5 number
 	    		procurementDetail.typeOfDurableGoods = Integer.parseInt(json.get("typeOfGoods").asText());
 	
@@ -1207,6 +1208,10 @@ public class Import extends Controller {
 			    			mc.remain=mc.remain+procurementDetail.quantity;
 			    			mc.pricePerEach=sumPrice/mc.remain;
 			    			mc.update();
+			    			
+			    			System.out.println("After");
+			    			System.out.println(mc.remain);
+				    		System.out.println(mc.pricePerEach);
 			    		}
 		    		}
 		    		else
@@ -1226,6 +1231,10 @@ public class Import extends Controller {
 				    			else
 				    				mc.pricePerEach=0;
 				    			mc.update();
+				    			
+				    			System.out.println("After");
+				    			System.out.println(mc.remain);
+					    		System.out.println(mc.pricePerEach);
 		    				}
 			    			
 			    			else if(procurementDetail.typeOfDurableGoods==0 && typeSub==0 )//เปลี่ยนค่าโดยที่วัสดุเป็นชนิดเดียวกัน /
@@ -1253,6 +1262,10 @@ public class Import extends Controller {
 				    			else
 				    				mc.pricePerEach=0;
 				    			mc.update();
+				    			
+				    			System.out.println("After");
+				    			System.out.println(mc.remain);
+					    		System.out.println(mc.pricePerEach);
 			    			}
 			    			else if(procurementDetail.typeOfDurableGoods==0 && typeSub==1 )//เปลี่ยนจากคงทนถาวรเป็นสิ้นเปลือง จะ+เพิ่ม /
 			    			{
@@ -1266,6 +1279,10 @@ public class Import extends Controller {
 				    			mc.remain=mc.remain+procurementDetail.quantity;
 				    			mc.pricePerEach=sumPrice/mc.remain;
 				    			mc.update();
+				    			
+				    			System.out.println("After");
+				    			System.out.println(mc.remain);
+					    		System.out.println(mc.pricePerEach);
 			    			}
 			    			
 		    		}
@@ -1274,90 +1291,6 @@ public class Import extends Controller {
 	    		System.out.println("\n\n Exception MaterialCode Not Found !!!! \n\n\n\n\n");
 	    	}
 	    	
-    		///Calculateeeeeeeeeeeeeeeeeee
-    		MaterialCode mc=null;
-    		
-    		System.out.println("Before");
-
-    			
-    		if(editingMode==false)//+ปกติ
-    		{
-    			if(procurementDetail.typeOfDurableGoods==0)	// เพิ่มตัวใหม่         /
-	    		{
-		    		mc= MaterialCode.find.byId(codeId);
-		    		System.out.println(mc.remain);
-		    		System.out.println(mc.pricePerEach);
-		    		
-	    			double sumPrice=mc.pricePerEach*mc.remain;
-	    			sumPrice=sumPrice+(procurementDetail.quantity*procurementDetail.price);
-	    			
-	    			mc.remain=mc.remain+procurementDetail.quantity;
-	    			mc.pricePerEach=sumPrice/mc.remain;
-	    			mc.update();
-	    		}
-    		}
-    		else
-    		{
-    				if(procurementDetail.typeOfDurableGoods==1 && typeSub==0 )//เปลี่ยนจากสิ้นเปลืองเป็นคงทนถาวร จะลบออก /
-    				{
-	    				mc= MaterialCode.find.byId(codeSub);
-	    	    		System.out.println(mc.remain);
-	    	    		System.out.println(mc.pricePerEach);
-			    		
-		    			double sumPrice=mc.pricePerEach*mc.remain;
-		    			sumPrice=sumPrice-(numSub*priceSub);
-		    			
-		    			mc.remain=mc.remain-numSub;
-		    			if(mc.remain>0)
-		    				mc.pricePerEach=sumPrice/mc.remain;
-		    			else
-		    				mc.pricePerEach=0;
-		    			mc.update();
-    				}
-	    			
-	    			else if(procurementDetail.typeOfDurableGoods==0 && typeSub==0 )//เปลี่ยนค่าโดยที่วัสดุเป็นชนิดเดียวกัน /
-	    			{
-	    				mc= MaterialCode.find.byId(codeId);
-	    	    		System.out.println(mc.remain);
-	    	    		System.out.println(mc.pricePerEach);
-			    		
-		    			double sumPrice=mc.pricePerEach*mc.remain;
-		    			sumPrice=sumPrice-(numSub*priceSub);
-		    			
-		    			mc.remain=mc.remain-numSub;
-		    			if(mc.remain>0)
-		    				mc.pricePerEach=sumPrice/mc.remain;
-		    			else
-		    				mc.pricePerEach=0;
-		    			//////////////////////////////////////////
-		    			sumPrice=mc.pricePerEach*mc.remain;
-		    			sumPrice=sumPrice+(procurementDetail.quantity*procurementDetail.price);
-		    			
-		    			mc.remain=mc.remain+procurementDetail.quantity;
-		    			
-		    			if(mc.remain>0)
-		    				mc.pricePerEach=sumPrice/mc.remain;
-		    			else
-		    				mc.pricePerEach=0;
-		    			mc.update();
-	    			}
-	    			else if(procurementDetail.typeOfDurableGoods==0 && typeSub==1 )//เปลี่ยนจากคงทนถาวรเป็นสิ้นเปลือง จะ+เพิ่ม /
-	    			{
-			    		mc= MaterialCode.find.byId(codeId);
-			    		System.out.println(mc.remain);
-			    		System.out.println(mc.pricePerEach);
-			    		
-		    			double sumPrice=mc.pricePerEach*mc.remain;
-		    			sumPrice=sumPrice+(procurementDetail.quantity*procurementDetail.price);
-		    			
-		    			mc.remain=mc.remain+procurementDetail.quantity;
-		    			mc.pricePerEach=sumPrice/mc.remain;
-		    			mc.update();
-	    			}
-	    			
-    		}
-
-	    		///Calculateeeeeeeeeeeeeeeeeee
 	    	
 	    	if(!editingMode) procurementDetail.save();
 	    	else procurementDetail.update();
