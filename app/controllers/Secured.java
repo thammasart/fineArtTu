82f5d28be6cd196dd63055f765b4daa3b4aa0d82
soranad <oldman.beta@gmail.com>
2014-11-12 11:41:45 +0700

    fix bug have session but no user in database

--- app/controllers/Secured.java
+++ app/controllers/Secured.java
@@ -9,6 +9,10 @@ public class Secured extends Security.Authenticator {
 
     @Override
     public String getUsername(Context ctx) {
+    	User user = User.find.byId(ctx.session().get("username"));
+        if(user == null){
+        	ctx.session().clear();
+        }
         return ctx.session().get("username");
     }
 
