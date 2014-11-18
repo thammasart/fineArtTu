8fb1ab5da0d9ab3cdc4dda7f061b15ac5a64386c
peat2110 <pppppooo@hotmail.com>
2014-11-18 17:35:04 +0700

    for pull

--- app/controllers/Report.java
+++ app/controllers/Report.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Collections.*;
+import java.util.Locale;
 import java.text.*;
 
 public class Report  extends Controller {
@@ -33,10 +34,19 @@ public class Report  extends Controller {
         DynamicForm form = Form.form().bindFromRequest();
         String year = form.get("year");
 
-        return redirect(routes.Report.reportRemainingMaterial(Integer.parseInt(year)));
+        return redirect(routes.Report.reportRemainingMaterial(Integer.parseInt(year),1));
     }
     @Security.Authenticated(Secured.class)
-        public static Result reportRemainingMaterial(int year) {
+        public static Result reportRemainingMaterialPostPrint() {
+        User user = User.find.where().eq("username", session().get("username")).findUnique();
+        List<MaterialCode> mc = MaterialCode.find.all();
+        DynamicForm form = Form.form().bindFromRequest();
+        String year = form.get("yearPrint");
+
+        return redirect(routes.Report.reportRemainingMaterial(Integer.parseInt(year),2));
+    }
+    @Security.Authenticated(Secured.class)
+        public static Result reportRemainingMaterial(int year,int view) {
         
         if(year == -1){
             Date dNow = new Date( );
@@ -54,12 +64,18 @@ public class Report  extends Controller {
 
             int totalMaterial = 0;
             double totlePrice = 0.00;
+            int totalReciveAmount = 0;
+            double totalRecivePrice = 0;
+            int totalWithdrawAmount = 0;
+            double totalWithdrawPrice = 0;
 
             List<models.durableGoods.ProcurementDetail> allRemain = each.getProcurementDetailRemaining(year);
             for(models.durableGoods.ProcurementDetail remain : allRemain){
 
                 totalMaterial += remain.quantity;
                 totlePrice +=  remain.price * remain.quantity;
+                totalReciveAmount += remain.quantity;
+                totalRecivePrice += remain.price * remain.quantity;
 
                 String[] detail = new String[13];
                 if(details.size() == 0){
@@ -87,7 +103,7 @@ public class Report  extends Controller {
 
             List<models.durableGoods.ProcurementDetail> importDetails = models.durableGoods.ProcurementDetail.find.where().eq("code",each.code).eq("procurement.status",ImportStatus.SUCCESS).between("procurement.addDate",new Date(year-2444,9,1),new Date(year-2443,8,30)).orderBy("procurement.addDate asc").findList();
             List<models.durableGoods.RequisitionDetail> exportDetails = models.durableGoods.RequisitionDetail.find.where().eq("code",each).eq("requisition.status",ExportStatus.SUCCESS).between("requisition.approveDate",new Date(year-2444,9,1),new Date(year-2443,8,30)).orderBy("requisition.approveDate asc").findList();
-            SimpleDateFormat fts = new SimpleDateFormat ("dd.MM.yy");
+            SimpleDateFormat fts = new SimpleDateFormat ("dd/MM/yyyy",new Locale("th","th"));
             double totalAmount = temp.get(1);
 
             double sumOfPrice = temp.get(0) * temp.get(1);
@@ -96,6 +112,9 @@ public class Report  extends Controller {
 
                     totalMaterial += importDetails.get(0).quantity;
                     totlePrice += importDetails.get(0).price * importDetails.get(0).quantity; 
+
+                    totalReciveAmount += importDetails.get(0).quantity;
+                    totalRecivePrice += importDetails.get(0).price * importDetails.get(0).quantity;
 
                     String[] detail = new String[13];
                     if(details.size() == 0){
@@ -129,6 +148,9 @@ public class Report  extends Controller {
 
                     totalMaterial -= exportDetails.get(0).quantity;
                     totlePrice -= exportDetails.get(0).totlePrice;
+                    
+                    totalWithdrawAmount += exportDetails.get(0).quantity;
+                    totalWithdrawPrice += exportDetails.get(0).totlePrice;
 
                     String[] detail = new String[13];
                     if(details.size() == 0){
@@ -155,12 +177,15 @@ public class Report  extends Controller {
                     details.add(detail);
                     exportDetails.remove(0);
                 }
-            }
+            }// end while
 
             if(importDetails.size() > 0 ){
                 while(importDetails.size() > 0){     
                     totalMaterial += importDetails.get(0).quantity;
                     totlePrice += importDetails.get(0).price * importDetails.get(0).quantity; 
+
+                    totalReciveAmount += importDetails.get(0).quantity;
+                    totalRecivePrice += importDetails.get(0).price * importDetails.get(0).quantity;
 
                     String[] detail = new String[13];
                     if(details.size() == 0){
@@ -197,6 +222,9 @@ public class Report  extends Controller {
                     totalMaterial -= exportDetails.get(0).quantity;
                     totlePrice -= exportDetails.get(0).totlePrice;
 
+                    totalWithdrawAmount += exportDetails.get(0).quantity;
+                    totalWithdrawPrice += exportDetails.get(0).totlePrice;
+
                     String[] detail = new String[13];
                     if(details.size() == 0){
                         detail[0] = String.format("%d", material.size() + 1);
@@ -223,10 +251,34 @@ public class Report  extends Controller {
                     exportDetails.remove(0);
                 }
             }
+                if(!details.isEmpty()){
+                    //add conclusion
+                    String[] detail = new String[13];
+                    detail[0] = "";
+                    detail[1] = "ใช้ไป/คงเหลือ";
+                    detail[2] = "";
+                    detail[3] = "";
+                    detail[4] = "";
+                    detail[5] = "";
+                    detail[6] = "";
+                    detail[7] = String.format("%d", totalReciveAmount);
+                    detail[8] = String.format("%.2f", totalRecivePrice);
+                    detail[9] = String.format("%d", totalWithdrawAmount);
+                    detail[10] = String.format("%.2f", totalWithdrawPrice);
+                    detail[11] = String.format("%d", totalMaterial);
+                    detail[12] = String.format("%.2f", totlePrice);
+                    
+                    details.add(detail);
+                }
+
             material.add(details);
         }
 
-        return ok(reportRemainingMaterial.render(user,material));
+        if(view == 1){
+            return ok(reportRemainingMaterial.render(year,user,material));
+        } else {
+            return ok(reportRemainingMaterialPrint.render(year,user,material));
+        }
     }
     
     @Security.Authenticated(Secured.class)
